594ca5abb6b5349013c8d368231af1988c730388
ajm1230 <155264455+ajm1230@users.noreply.github.com>
2025-06-14 00:00:38 +0530

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,27 @@
+// firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/9.24.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.24.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAYq4RhrbHNoJlX4TP0PonQNuPGE5-nc58",
+    authDomain: "first-project-4ba00.firebaseapp.com",
+    databaseURL: "https://first-project-4ba00-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "first-project-4ba00",
+    storageBucket: "first-project-4ba00.appspot.com",
+    messagingSenderId: "679004245122",
+    appId: "1:679004245122:web:2b610906456030f74cf89b",
+    measurementId: "G-MPP649FMMM"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/icon.png'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
